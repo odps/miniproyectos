ffe37660402e2c30912f22a507e39634b7d7976e
Oscar Pereira <odps31@gmail.com>
2024-05-29 13:21:50 +0200

    Delete Pruebas directory

--- Pruebas/pruebas.js
+++
@@ -1,8 +0,0 @@
-let header = document.getElementById("h1");
-let nombre = document.getElementById("name");
-let boton = document.getElementById("b1");
-
-boton.addEventListener("click", () => {
-    header.innerText = `Hola ${nombre.value}`;
-    nombre.value = "";
-});

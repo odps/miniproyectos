fe8d157b06628047aa691d90982addd75617360c
Oscar Pereira <odps31@gmail.com>
2024-06-06 15:41:54 +0200

    Created simple number guessing game

---
+++ AdivinaElNumero/guess.js
@@ -0,0 +1,33 @@
+const userNum = document.getElementById("num");
+const boton = document.getElementById("boton");
+const feedback = document.getElementById("feedback");
+const maxIntentos = 5;
+let intentos = 0 ;
+
+const adivina = {
+    valor : Math.floor(Math.random()*10 +1)
+};
+
+boton.addEventListener("click", ()=>{
+    
+    intentos++;
+    let num = Number(userNum.value);
+    if(num === adivina.valor && intentos < maxIntentos){
+        feedback.innerText = "¡Ganaste!"
+        reintentar();
+    }else{
+        feedback.innerText = `Vuelve a intentarlo, te quedan ${maxIntentos - intentos} intentos`;
+    }
+
+    if (intentos == maxIntentos) {
+        reintentar();
+    }
+    
+});
+
+function reintentar() {
+    boton.value = "Intentar otra vez";
+    boton.onclick = ()=>{
+        location.reload();
+    };
+}
